12a22aa8ad4cebe1ab4ad81201bb6ff1f4f265f0
naveen <naveen.pendyala@gmail.com>
2024-04-13 11:28:09 +0530

    Added code in TEST_SETUP function

--- unity_project/Unity/my_examples/example_3/test/LedDriverTest.c
+++ unity_project/Unity/my_examples/example_3/test/LedDriverTest.c
@@ -1,12 +1,14 @@
 #include "unity_fixture.h"
 #include "LedDriver.h"
 
+static uint16_t virtualLeds;
 
 TEST_GROUP(LedDriver);
 
 TEST_SETUP(LedDriver)
 {
-
+    virtualLeds = 0xFFFF;
+    LedDriver_Init(&virtualLeds);
 }
 
 TEST_TEAR_DOWN(LedDriver)
@@ -16,27 +18,17 @@ TEST_TEAR_DOWN(LedDriver)
 
 TEST(LedDriver,Test_LedDriver_Init)
 {
-    uint16_t virtualLeds;
- 
-    virtualLeds = 0xFFFF;
-    LedDriver_Init(&virtualLeds);
     TEST_ASSERT_EQUAL_HEX16(0,virtualLeds);
 }
 
 TEST(LedDriver,Test_LedDriver_TurnOnLEDOne)
 {
-    uint16_t virtualLeds;
-
-    LedDriver_Init(&virtualLeds);
     LedDriver_TurnOn(1);
     TEST_ASSERT_EQUAL_HEX16(1,virtualLeds);
 }
 
 TEST(LedDriver,Test_LedDriver_TurnOffLEDOne)
 {
-    uint16_t virtualLeds;
-
-    LedDriver_Init(&virtualLeds);
     LedDriver_TurnOn(1);
     LedDriver_TurnOff(1);
     TEST_ASSERT_EQUAL_HEX16(0,virtualLeds);
